90fd5395c53cb670ee372b329028409beff140b0
Zsolt Branyiczky <brazso@zematix.hu>
2022-01-06 23:01:43 +0100

    add dropdowns to favourite-team component

--- src/app/feature/favourite-team/favourite-team.component.ts
+++ src/app/feature/favourite-team/favourite-team.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 import { TranslocoService } from '@ngneat/transloco';
-import { SessionService } from 'src/app/core/services';
-import { UiError } from 'src/app/core/models';
+import { ApiService, SessionService } from 'src/app/core/services';
+import { GenericListResponse, SessionData, Team, UiError } from 'src/app/core/models';
 import { default as RouterUrls} from 'src/app/core/constants/router-urls.json';
+import { default as ApiEndpoints } from 'src/app/core/constants/api-endpoints.json';
+import { forkJoin } from 'rxjs';
 
 @Component({
   // selector: 'app-favourite-team',
@@ -14,14 +15,38 @@ export class FavouriteTeamComponent implements OnInit {
 
   isSubmitting = false;
   errors: UiError = new UiError({});
+  session: SessionData;
+  groupTeams: Team[];
+  selectedGroupTeam: Team | undefined;
+  knockOutTeams: Team[];
+  selectedKnockOutTeam: Team | undefined;
   
   constructor(
     // private readonly router: Router,
-    private readonly translocoService: TranslocoService,
+    // private readonly translocoService: TranslocoService,
     private readonly sessionService: SessionService,
+    private readonly apiService: ApiService
   ) { }
 
   ngOnInit(): void {
+    this.sessionService.session.subscribe(
+      (session: SessionData) => {
+        this.session = session;
+        console.log(`session: ${JSON.stringify(session)}`);
+
+        this.selectedGroupTeam = session.userOfEvent?.favouriteGroupTeam;
+        this.selectedKnockOutTeam = session.userOfEvent?.favouriteKnockoutTeam;
+
+        forkJoin([
+          this.apiService.get<GenericListResponse<Team>>(`${ApiEndpoints.TEAMS.FIND_FAVOURITE_GROUP_TEAMS_BY_EVENT}?eventId=${session.event?.eventId}`),
+          this.apiService.get<GenericListResponse<Team>>(`${ApiEndpoints.TEAMS.FIND_FAVOURITE_KNOCK_OUT_TEAMS_BY_EVENT}?eventId=${session.event?.eventId}`)
+          ]).subscribe(([groupTeamsResponse, knockOutTeamsResponse]) => {
+            this.groupTeams = groupTeamsResponse.data;
+            this.knockOutTeams = knockOutTeamsResponse.data;
+          }
+        );
+      }
+    );
   }
 
   doSave(event_: any): void {
