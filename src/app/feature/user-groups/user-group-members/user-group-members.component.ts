f2adfd72ca198e2f7e4355683f001f800552b77b
Zsolt Branyiczky <brazso@zematix.hu>
2022-04-05 14:34:37 +0200

    dev on adding a member to the selected user group

--- src/app/feature/user-groups/user-group-members/user-group-members.component.ts
+++ src/app/feature/user-groups/user-group-members/user-group-members.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ChangeDetectionStrategy, Input, ViewChild, SimpleCha
 import { NgForm } from '@angular/forms';
 import { TranslocoService } from '@ngneat/transloco';
 import { ConfirmationService } from 'primeng/api';
-import { CommonResponse, GenericListResponse, SessionData, UiError, User, UserGroup } from 'src/app/core/models';
+import { CommonResponse, GenericListResponse, GenericResponse, SessionData, UiError, User, UserGroup } from 'src/app/core/models';
 import { ApiService, SessionService } from 'src/app/core/services';
 import { ReplaceLineBreaksPipe } from 'src/app/shared/pipes/replace-line-breaks.pipe';
 import { default as ApiEndpoints } from 'src/app/core/constants/api-endpoints.json';
@@ -21,6 +21,7 @@ export class UserGroupMembersComponent implements OnInit {
   userGroupMembers: User[];
   selectedUserGroupMember: User | undefined;
   isUserGroupMemberDialogDisplayed: boolean = false;
+  userGroupMemberDialogErrors: UiError = new UiError({});
   userGroupMember: User = {} as User; // added one
   userGroupMemberLoginNameResults: string[];
   userGroupMemberFullNameResults: string[];
@@ -44,7 +45,7 @@ export class UserGroupMembersComponent implements OnInit {
     );  }
 
   ngOnChanges(changes: SimpleChanges) {
-    // console.log(`ngOnChanges: ${JSON.stringify(changes)}`);
+    console.log(`ngOnChanges: ${JSON.stringify(changes)}`);
     // this.doSomething(changes.categoryId.currentValue);
     //   // You can also use categoryId.previousValue and 
     //   // categoryId.firstChange for comparing old and new values
@@ -67,9 +68,11 @@ export class UserGroupMembersComponent implements OnInit {
   }
 
   doAdd(event_: any): void {
+    console.log(`doAdd`);
     // this.userGroup = {} as UserGroup;
     // this.userGroupForm.resetForm();
     // this.displayedComponentEnum = DisplayedComponentEnum.USER_GROUP_DIALOG;
+    this.isUserGroupMemberDialogDisplayed = true;
   }
 
   doRemove(event_: any): void {
@@ -83,55 +86,53 @@ export class UserGroupMembersComponent implements OnInit {
   }
 
   userGroupMemberLoginNameComplete(event_: any): void {
-    // this.mylookupservice.getResults(event.query).then(data => {
-    //     this.results = data;
-    // });
+    // console.log(`event_: ${JSON.stringify(event_)}`);
+    this.apiService.get<GenericListResponse<string>>(`${ApiEndpoints.USERS.FIND_USER_LOGIN_NAMES_BY_PREFIX}?loginNamePrefix=${event_.query}`).subscribe(
+      (value) => {
+        this.userGroupMemberLoginNameResults = value.data;
+      }
+    );
   }
 
   userGroupMemberFullNameComplete(event_: any): void {
-    // this.mylookupservice.getResults(event.query).then(data => {
-    //     this.results = data;
-    // });
+    this.apiService.get<GenericListResponse<string>>(`${ApiEndpoints.USERS.FIND_USER_FULL_NAMES_BY_CONTAIN}?fullNameContain=${event_.query}`).subscribe(
+      (value) => {
+        this.userGroupMemberFullNameResults = value.data;
+      }
+    );
   }
 
   doAddUserGroupMember(event_: any): void {
     console.log(`doAddUserGroupMember`);
-    // console.log(`event_: ${JSON.stringify(event_)}`);
-    // this.isSubmitting = true;
+    console.log(`event_: ${JSON.stringify(event_)}`);
+    this.isSubmitting = true;
 
-    // this.apiService.post<GenericResponse<UserGroup>>(ApiEndpoints.USER_GROUPS.INSERT_USER_GROUP+'?eventId={0}&userId={1}&name={2}&isInsertConfirmed={3}'
-    //   .format(this.sessionService.getEvent().eventId, this.sessionService.getUser().userId, this.userGroup.name, isImportConfirmed)).subscribe({
-    //   next: value => {
-    //     console.log('inserted');
-    //     const insertedUserGroup: UserGroup  = value.data;
-    //     this.userGroups.push(insertedUserGroup);
-    //     this.selectedUserGroup = insertedUserGroup;
-    //     this.displayedComponentEnum = DisplayedComponentEnum.USER_GROUP_COMPONENT;
-    //   },
-    //   error: (err: HttpErrorResponse) => {
-    //     console.log(`err: ${JSON.stringify(err)}`);
-    //     let error = err.error;
-    //     if (error && isApiError(error) && error.items && getApiErrorOverallType(error) === ParameterizedMessageTypeEnum.INFO 
-    //       && error.items.some(e => e.msgCode === 'USER_GROUP_NAME_OCCUPIED_ON_EARLIER_EVENT')) {
-    //         let errorItem = error.items.find(e => e.msgCode === 'USER_GROUP_NAME_OCCUPIED_ON_EARLIER_EVENT');
-    //         // console.log(`errorItem: ${JSON.stringify(errorItem)}`);
-    //         this.confirmMsg = apiErrorItemMsgFormat(errorItem!, this.translocoService.translate(errorItem!.msgCode));
-    //         // console.log(`confirmMsg: ${confirmMsg}`);
-    //         this.displayedComponentEnum = DisplayedComponentEnum.IMPORT_CONFIRM_DIALOG;
-    //     }
-    //     else {
-    //       this.errors = new UiError(Object.assign(err));
-    //     }
-    //   },
-    //   complete: () => {
-    //     console.log('complete');
-    //     this.isSubmitting = false;
-    //   }
-    // });
+    console.log(`userGroupMember: ${JSON.stringify(this.userGroupMember)}`);
+    this.apiService.post<GenericResponse<User>>(ApiEndpoints.USER_GROUPS.FIND_AND_ADD_USER_TO_USER_GROUP+'?userGroupId={0}&loginName={1}&fullName={2}'
+      .format(this.selectedUserGroup!.userGroupId, this.userGroupMember!.loginName ?? '', this.userGroupMember!.fullName ?? '')).subscribe({
+      next: value => {
+        console.log('added');
+        const addedUser: User  = value.data;
+        console.log(`addedUser: ${JSON.stringify(addedUser)}`);
+        this.userGroupMembers.push(addedUser);
+        this.selectedUserGroupMember = addedUser;
+        this.doResetUserGroupMember(event_);
+      },
+      error: (err: HttpErrorResponse) => {
+        console.log(`err: ${JSON.stringify(err)}`);
+        this.userGroupMemberDialogErrors = new UiError(Object.assign(err));
+        this.isSubmitting = false;
+      },
+      complete: () => {
+        console.log('complete');
+        this.isSubmitting = false;
+      }
+    });
   }
 
   doResetUserGroupMember(event_: any): void {
-    // this.displayedComponentEnum = DisplayedComponentEnum.USER_GROUP_COMPONENT;
+    this.isUserGroupMemberDialogDisplayed = false;
+    this.userGroupMemberDialogErrors = new UiError({});
   }
 
   doRemoveUserGroupMember(event_: any): void {
