35805b7d5be509f9d5ab8670fa6013526f6392c3
Zsolt Branyiczky <brazso@zematix.hu>
2022-04-12 17:24:23 +0200

    some comments were modified

--- src/app/core/models/common/genericMapResponse.model.ts
+++ src/app/core/models/common/genericMapResponse.model.ts
@@ -1,6 +1,6 @@
 import { CommonResponse } from '.';
 
-export interface GenericMapResponse</*K = string | number | boolean,*/ T = any> extends CommonResponse {
-//   data: Map<K, T>; // cannot be used here because dictionary comes in rest repsonse
-  data: { [key: string]: T }; // index signature (key type) can be only string | number | boolean and not generic
+export interface GenericMapResponse</*K = string | number | symbol,*/ T = any> extends CommonResponse {
+//   data: Map<K, T>; // cannot be used here because object dictionary comes in rest repsonse not a map
+  data: { [key: string]: T }; // index signature (key type) can be only string | number | symbol but not a generic
 }
