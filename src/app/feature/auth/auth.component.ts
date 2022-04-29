20a3d3910d5a6194b75c0f1d0b4a98b301f43211
Zsolt Branyiczky <brazso@zematix.hu>
2022-04-29 17:36:30 +0200

    user registration

--- src/app/feature/auth/auth.component.ts
+++ src/app/feature/auth/auth.component.ts
@@ -1,11 +1,11 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, Validators, ValidatorFn, ValidationErrors, AbstractControl } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { LangDefinition, Translation, TranslocoService } from '@ngneat/transloco';
-
-import { SessionService, UiError } from 'src/app/core';
+import { ApiService, GenericResponse, SessionService, UiError, User, UserExtended } from 'src/app/core';
 import { default as RouterUrls} from 'src/app/core/constants/router-urls.json';
+import { default as ApiEndpoints } from 'src/app/core/constants/api-endpoints.json';
 
 @Component({
   selector: 'app-auth-page',
@@ -25,12 +25,13 @@ export class AuthComponent implements OnInit {
     private router: Router,
     private translocoService: TranslocoService,
     private sessionService: SessionService,
+    private apiService: ApiService,
     private fb: FormBuilder
   ) {
     // use FormBuilder to create a form group
     this.authForm = this.fb.group({
       'username': ['', Validators.required],
-      'password': ['', Validators.required],
+      'password': ['', [Validators.required, Validators.minLength(8)]],
       'language': ['']
     });
   }
@@ -45,9 +46,12 @@ export class AuthComponent implements OnInit {
     this.route.url.subscribe(data => {
       // Get the last piece of the URL (it's either 'login' or 'register')
       this.authType = data[data.length - 1].path;
-      // add form control for username if this is the register page
+      // add additional form controls if this is the register page
       if (this.authType === 'register') {
-        this.authForm.addControl('email', new FormControl());
+        this.authForm.addControl('passwordAgain', new FormControl('', [Validators.required, Validators.minLength(8)]));
+        this.authForm.addControl('fullName', new FormControl('', [Validators.required]));
+        this.authForm.addControl('email', new FormControl('', [Validators.required, this.validateEmail]));
+        this.authForm.addValidators(this.validatePasswords);
       }
 
       // wait until translation is being loaded
@@ -63,24 +67,66 @@ export class AuthComponent implements OnInit {
     this.errors = new UiError({});
 
     const credentials = this.authForm.value;
-    console.log(`credentials: ${JSON.stringify(credentials)}`)
-    this.sessionService
-      .attemptAuth(this.authType, credentials)
-      .subscribe({
-        next: session => {
-          this.router.navigate([RouterUrls.HOME_PAGE]);
+    console.log(`credentials: ${JSON.stringify(credentials)}`);
+
+    if (this.authType === 'register') {
+      const user: UserExtended = {
+        loginName: credentials['username'],
+        loginPasswordNew: credentials['password'],
+        loginPasswordAgain: credentials['passwordAgain'],
+        fullName: credentials['fullName'],
+        emailAddr: credentials['email'],
+        zoneId: Intl.DateTimeFormat().resolvedOptions().timeZone,
+        languageTag: credentials['language']
+      } as UserExtended;
+      console.log(`user: ${JSON.stringify(user)}`);
+      this.isSubmitting = false;
+      this.apiService.post<GenericResponse<User>>(ApiEndpoints.SIGNUP, user).subscribe({
+        next: value => {
+          let savedUser: User = value.data;
+          console.log(`savedUser: ${JSON.stringify(savedUser)}`);
+          this.isSubmitting = false;
         },
         error: (err: HttpErrorResponse) => {
           console.log(`err: ${JSON.stringify(err)}`);
           this.errors = new UiError(Object.assign(err));
           this.isSubmitting = false;
+        },
+        complete: () => {
+          console.log('complete');
         }
       });
+    }
+    else {
+      this.sessionService
+        .attemptAuth(this.authType, credentials)
+        .subscribe({
+          next: session => {
+            this.router.navigate([RouterUrls.HOME_PAGE]);
+          },
+          error: (err: HttpErrorResponse) => {
+            console.log(`err: ${JSON.stringify(err)}`);
+            this.errors = new UiError(Object.assign(err));
+            this.isSubmitting = false;
+          }
+        });
+    }
   }
 
   onLanguageChange(event: any): void {
     const lang: string = event.value;
     console.log(`setActiveLang2: ${lang}`);
     this.translocoService.setActiveLang(lang);
+  }
+
+  validateEmail(control: FormControl): ValidationErrors | null {
+    const email = control.value;
+    console.log(`email: ${email}`);
+    return !email || /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email) ? null : {email: {value: control.value}};
+  }
+
+  validatePasswords(form: /*FormGroup*/ AbstractControl): ValidationErrors | null {
+    const isValid: boolean = (form.get("password")!.value ?? '') === (form.get("passwordAgain")!.value ?? '');
+    return isValid ? null : {passwords: true};
   }
 }
