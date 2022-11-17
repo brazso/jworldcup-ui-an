8f412ed244a642bf084d5c963f74322a8e7c8cc7
Zsolt Branyiczky <brazso@zematix.hu>
2022-11-17 11:07:25 +0100

    modified comment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   be_root: "https://worldcup.zematix.hu/jworldcup-api",
   be_socket: "wss://worldcup.zematix.hu/ws",
   be_socket_user: "jworldcup",
-  be_socket_passcode: "jworldcup", // being overwritten in docker
+  be_socket_passcode: "jworldcup", // being overwritten in docker/frontend/Dockerfile but it is still a security hole!
   with_credentials: true,
   recaptcha_site_key: "6LcEctMiAAAAAKzVnpnO64slgTWVEpMSkm3_aGC8"
 };
