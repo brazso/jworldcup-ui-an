15b7a9ca360965ca28da31d07a54437036e0b241
Zsolt Branyiczky <brazso@zematix.hu>
2022-11-18 19:11:34 +0100

    fix: suppress console methods only in production mode

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-  // suppress used console functions except error one in production mode
-  console.debug = ()=>{};
-  // console.error = ()=>{};
-  console.log = ()=>{};
-  console.trace = ()=>{};
-  console.warn = ()=>{};
-
 if (environment.production) {
   enableProdMode();
 
