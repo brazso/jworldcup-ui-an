f6f274e2e2c29ce75af5108b2e8508dc6449a678
Zsolt Branyiczky <brazso@zematix.hu>
2022-11-18 15:57:12 +0100

    suppress used console function in production mode

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,22 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+  // suppress used console functions except error one in production mode
+  console.debug = ()=>{};
+  // console.error = ()=>{};
+  console.log = ()=>{};
+  console.trace = ()=>{};
+  console.warn = ()=>{};
+
 if (environment.production) {
   enableProdMode();
+
+  // suppress used console functions except error one in production mode
+  console.debug = ()=>{};
+  // console.error = ()=>{};
+  console.log = ()=>{};
+  console.trace = ()=>{};
+  console.warn = ()=>{};
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
